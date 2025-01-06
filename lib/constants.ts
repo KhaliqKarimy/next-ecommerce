ea319ac9108df6dcb9c759626f6009892e7ff4be
khaliqkarimi <khlqkarimy@gmail.com>
2025-01-06 12:00:25 +0330

    03-create-website-layout

---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'NxtAmzn'
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy more'
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'An Amzone clone build with Next.js and Mongodb'
+
+
+// export const USER_ROLES = ['Admin', 'User']
+// export const COLORS = ['Gold', 'Green', 'Red']
+// export const THEMES = ['Light', 'Dark', 'System']
