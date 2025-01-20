dafad27ace59a7fa731ababea463a6bc37e75602
khaliqkarimi <khlqkarimy@gmail.com>
2025-01-20 15:22:52 +0330

    final-code

--- types/index.ts
+++ types/index.ts
@@ -44,8 +44,16 @@ export type Data = {
     isPublished: boolean;
   }[];
 };
-
+// Order
 export type IOrderInput = z.infer<typeof OrderInputSchema>;
+export type IOrderList = IOrderInput & {
+  _id:string
+  user:{
+    name:string
+    email:string
+  }
+  createdAt: Date
+}
 export type OrderItem = z.infer<typeof OrderItemSchema>;
 export type Cart = z.infer<typeof CartSchema>;
 export type ShippingAddress = z.infer<typeof ShippingAddressSchema>;
@@ -55,3 +63,5 @@ export type IUserInput = z.infer<typeof UserInputSchema>;
 export type IUserSignIn = z.infer<typeof UserSignInSchema>;
 export type IUserSignUp = z.infer<typeof UserSignUpSchema>;
 export type IUserName = z.infer<typeof UserNameSchema>;
+
+
